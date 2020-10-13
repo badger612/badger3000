35d0158ae2b5d61a60c23194fa3f11cf38c7eb24
Kyle Ross <kyle@badger3000.com>
2020-10-13 13:26:30 -0700

    Revert "Revert "updates to Sanity intergrations""
    This reverts commit e18cb1dc4d4604a5867325f0c68dc16f42545ee6.

---
+++ web/gatsby-config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Kyle Ross - Web Developer',
+    author: 'Kyle Ross',
+    description: 'Personal site of Kyle Ross',
+  },
+  plugins: [
+    'gatsby-plugin-preact',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `48q0dqi8`,
+        dataset: `production`,
+        // a token with read permissions is required
+        // if you have a private dataset
+        //token: process.env.SANITY_TOKEN,
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: 'default',
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Badger3000',
+        short_name: 'Badger3000',
+        start_url: '/',
+        background_color: '#49bf9d',
+        theme_color: '#49bf9d',
+        display: 'standalone',
+        icon: 'src/assets/images/website-icon.png',
+        icon_options: {
+          // For all the options available, please see the additional resources below.
+          purpose: `any maskable`,
+        },
+        orientation: 'portrait', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: [
+          'https://samsungnext.com/',
+          'https://www.hotwire.com/hotels/',
+          'https://www.sony.co.uk/',
+          'https://www.youtube.com/',
+          'https://github.com/',
+          'https://www.linkedin.com/',
+        ],
+      },
+    },
+    'gatsby-plugin-preload-link-crossorigin',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+  ],
+}
